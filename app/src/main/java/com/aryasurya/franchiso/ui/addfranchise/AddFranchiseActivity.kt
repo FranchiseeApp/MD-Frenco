f33ccf26c7ec857b38b33f389f1967cf469ff860
AryaSurya <suryasaputra914@gmail.com>
2023-12-06 21:01:25 +0700

    update addfranchise

--- app/src/main/java/com/aryasurya/franchiso/ui/addfranchise/AddFranchiseActivity.kt
+++ app/src/main/java/com/aryasurya/franchiso/ui/addfranchise/AddFranchiseActivity.kt
@@ -2,11 +2,20 @@ package com.aryasurya.franchiso.ui.addfranchise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.aryasurya.franchiso.R
+import com.aryasurya.franchiso.databinding.ActivityAddFranchiseBinding
 
 class AddFranchiseActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddFranchiseBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_franchise)
+        binding = ActivityAddFranchiseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val items = listOf("Food", "Drink")
+        val adapter = ArrayAdapter(this@AddFranchiseActivity, R.layout.text_type_franchise, items)
+        binding.autoCompleteTextView.setAdapter(adapter)
     }
 }

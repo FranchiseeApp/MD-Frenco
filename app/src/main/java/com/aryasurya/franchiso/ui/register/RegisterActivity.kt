072881b631bad798706375513e3c299ce50e0691
AryaSurya <suryasaputra914@gmail.com>
2023-12-06 13:12:20 +0700

    Add RegisterActivity

---
+++ app/src/main/java/com/aryasurya/franchiso/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.aryasurya.franchiso.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aryasurya.franchiso.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}

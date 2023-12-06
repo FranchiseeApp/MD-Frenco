e6d94beaebf379fb63c0ec3af8c04827c85ab0de
AryaSurya <suryasaputra914@gmail.com>
2023-12-06 13:23:53 +0700

    Add MainActivity

--- app/src/main/java/com/aryasurya/franchiso/MainActivity.kt
+++ app/src/main/java/com/aryasurya/franchiso/MainActivity.kt
@@ -2,10 +2,31 @@ package com.aryasurya.franchiso
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.aryasurya.franchiso.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            setDisplayShowTitleEnabled(true)
+        }
+
+        val navView: BottomNavigationView = binding.navView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navView.setupWithNavController(navController)
+
     }
 }
---
+++ app/src/main/java/com/aryasurya/franchiso/ui/addfranchise/AddFranchiseActivity.kt
@@ -0,0 +1,12 @@
+package com.aryasurya.franchiso.ui.addfranchise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aryasurya.franchiso.R
+
+class AddFranchiseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_franchise)
+    }
+}

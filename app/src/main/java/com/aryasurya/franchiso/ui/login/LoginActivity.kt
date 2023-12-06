edf45bd502274ecc180a2b1101edfccb5becdae7
AryaSurya <suryasaputra914@gmail.com>
2023-12-06 21:26:16 +0700

    Memberikan intent

--- app/src/main/java/com/aryasurya/franchiso/ui/login/LoginActivity.kt
+++ app/src/main/java/com/aryasurya/franchiso/ui/login/LoginActivity.kt
@@ -1,12 +1,27 @@
 package com.aryasurya.franchiso.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aryasurya.franchiso.MainActivity
 import com.aryasurya.franchiso.R
+import com.aryasurya.franchiso.databinding.ActivityLoginBinding
+import com.aryasurya.franchiso.ui.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvNotHaveAcc.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+        }
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/aryasurya/franchiso/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/aryasurya/franchiso/ui/register/RegisterActivity.kt
@@ -1,12 +1,22 @@
 package com.aryasurya.franchiso.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aryasurya.franchiso.R
+import com.aryasurya.franchiso.databinding.ActivityRegisterBinding
+import com.aryasurya.franchiso.ui.login.LoginActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvLoginHere.setOnClickListener {
+            startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+        }
     }
 }
